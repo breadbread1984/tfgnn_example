e099b8ea16eca4abef73e68f93b21660423c3a2b
breadbread1984 <breadbread1984@163.com>
2024-02-06 10:25:38 +0800

    update

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import tensorflow_gnn as tfgnn
 from create_datasets import graph_tensor_spec
 
 def FeatureExtract(channels = 256, layer_num = 4, drop_rate = 0.5, node_types = 118, edge_types = 22):
-  inputs = tf.keras.Input(graph_tensor_spec())
+  inputs = tf.keras.Input(type_spec = graph_tensor_spec())
   results = inputs.merge_batch_to_components() # merge graphs of a batch to one graph as different components
   results = tfgnn.keras.layers.MapFeatures(
     node_sets_fn = lambda node_set, *, node_set_name: tf.keras.layers.Embedding(node_types, channels)(node_set[tfgnn.HIDDEN_STATE]),
@@ -38,7 +38,7 @@ def FeatureExtract(channels = 256, layer_num = 4, drop_rate = 0.5, node_types = 
   return tf.keras.Model(inputs = inputs, outputs = results)
 
 def Predictor(channels = 256, layer_num = 4, drop_rate = 0.5, node_types = 118, edge_types = 22):
-  inputs = tf.keras.Input(graph_tensor_spec())
+  inputs = tf.keras.Input(type_spec = graph_tensor_spec())
   results = FeatureExtract(channels, layer_num, drop_rate, node_types, edge_types)(inputs)
   results = tf.keras.layers.Dense(1)(results)
   return tf.keras.Model(inputs = inputs, outputs = results)
